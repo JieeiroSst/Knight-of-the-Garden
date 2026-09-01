25c7052a8637fb6550884b659c600521bc796e29
Qian <luumanhquan.91@gmail.com>
2026-09-02 03:09:57 +0700

    setting lang vn or eng

--- .godot/mono/temp/obj/Debug/HiepSiVeVuon.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/HiepSiVeVuon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HiepSiVeVuon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+305d35ca8a3ba99e5d5f16dba6f244d98144884a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73dd26acab417874363ba9f930308472308dc3d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HiepSiVeVuon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HiepSiVeVuon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
