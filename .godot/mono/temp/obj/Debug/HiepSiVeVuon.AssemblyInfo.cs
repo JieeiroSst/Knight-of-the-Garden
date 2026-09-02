2f4b5930c715fd0dc50cdf775609d93a46e233c2
Qian <luumanhquan.91@gmail.com>
2026-09-03 02:24:54 +0700

    fix bug

--- .godot/mono/temp/obj/Debug/HiepSiVeVuon.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/HiepSiVeVuon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HiepSiVeVuon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a7df0de089988754b97c0a2cb2308c27385a09b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e8135c195a2e175be7f71a6dc5808137db8e4d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HiepSiVeVuon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HiepSiVeVuon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
